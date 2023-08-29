feb80b93c7bed292d4f4dc8d958435d85bf8256b
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2023-08-29 17:41:23 +0200

    Add form at the end with validation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,19 @@ import '../styles/Footer.css'
 
 function Footer() {
 	const [inputValue, setInputValue] = useState('')
+  const isInputError =! inputValue.includes('@')
 
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
-			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<div className='lmj-footer-elem' onBlur={() => isInputError &&
+        alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide.")
+      }>Laissez-nous votre mail :
+        <textarea value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <button onClick={() => alert(inputValue)}>Envoyer</button>
+      </div>
 		</footer>
 	)
 }
