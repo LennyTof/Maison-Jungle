e76791287fe377e3e3c01e11f6bdc221fd1ff25c
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2023-08-29 16:51:33 +0200

    Add event to show water or sun needed for plant

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,9 +1,26 @@
+import Sun from '../assets/sun.svg'
+import Water from '../assets/water.svg'
+
+function Need(sunNeed) {
+
+}
+
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
-	const scaleType = careType === 'light' ? '☀️' : '💧'
+	const scaleType = careType === 'light' ? ( <img src={Sun} alt='sun-icon' /> ) : ( <img src={Water} alt='sun-icon' /> )
 
 	return (
-		<div>
+		<div onClick={() => {
+      let quantity = ""
+      const needed = careType === 'light' ? 'de lumière' : "d'arrosage"
+      if (scaleValue === 1) {
+       quantity = 'peu'
+      } else if (scaleValue === 2) {
+        quantity = 'modérement'
+      } else {
+        quantity = 'beaucoup'
+      }
+      alert(`cette plante requiert ${quantity} ${needed}`)}}>
 			{range.map((rangeElem) =>
 				scaleValue >= rangeElem ? (
 					<span key={rangeElem.toString()}>{scaleType}</span>
